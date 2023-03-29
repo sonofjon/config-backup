8160d84727f3aba7ebbda59e9c8b627e37814325
Andreas Jonsson <ajdev8@gmail.com>
2023-03-29 19:24:36 +0200

    Update WSL backup script after import

--- wsl/backup.sh
+++ wsl/backup.sh
@@ -14,13 +14,13 @@
 #
 # or symlink in anacron directory, e.g.
 #
-#   ln -s ~/dotfiles/config-wsl/backup.sh /etc/cron.hourly/backup
+#   ln -s ~/dotfiles/config-backup/wsl/backup.sh /etc/cron.hourly/backup
 #
 
 WINUSER=$(powershell.exe '$env:UserName' | tr -d '\r')
 SOURCE=$HOME/
 DEST=/mnt/c/Users/"$WINUSER"/Backup/wsl/
-EXCLUDE_FILE=$HOME/dotfiles/config-wsl/exclude.rsync
+EXCLUDE_FILE=$HOME/dotfiles/config-backup/wsl/exclude.rsync
 LOG_FILE=$HOME/backup.log
 
 rsync -a --delete $@ --exclude-from=$EXCLUDE_FILE "$SOURCE" "$DEST" --log-file="$LOG_FILE"
